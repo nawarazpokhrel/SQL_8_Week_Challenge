d02fc68b5fa1e8de00a798a30dd69259df8773d6
nawarazpokhrel <pokhrelnawaraz@yahoo.com>
2021-05-28 22:35:34 +0545

    solve remaining questions

--- week1/week_1.sql
+++ week1/week_1.sql
@@ -11,3 +11,20 @@ ORDER BY s.customer_id;
 "A"	            76
 "B"	            74
 "C"	            36
+
+
+
+SELECT customer_id, COUNT(DISTINCT order_date) as "DAYS VISITED" from sales GROUP BY customer_id;
+
+"customer_id"	"DAYS VISITED"
+"A"	4
+"B"	6
+"C"	2
+
+
+
+4.What is the most purchased item on the menu and how many times was it purchased by all customers?
+SELECT m.product_name,count (*)  FROM menu m Inner join sales s On s.product_id = m.product_id Group BY m.product_name order by count (*) desc limit 1;
+
+"product_name"	"count"
+"ramen"	           8
